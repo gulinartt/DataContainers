f6608ae3fced39bd976e4ae6191a747a049c5578
Enid <Enid@DESKTOP-DBBG6DL>
2023-04-06 19:59:31 +0500

    Add project files.

---
+++ DataContainers/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+#define POINTERS_AND_ARRAYS
+
+void main()
+{
+	setlocale(LC_ALL, " ");
+
+	int n;
+	cout << "¬ведите размер массива: "; cin >> n;
+	int* arr = new int[n];
+
+	
+}
